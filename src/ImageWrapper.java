8117fd878ff42aab909a0b72c5292214c3b6b94c
Chaitanya Ravuri <chaitu.ravuri.10@gmail.com>
2019-12-08 09:54:42 -0800

    Fixed comments of DibDump and ImageWrapper

--- src/ImageWrapper.java
+++ src/ImageWrapper.java
@@ -1,18 +1,26 @@
-
-/*
- * Created by cravuri on 10/24/19
- */
-
 /**
+ * Image Wrapper
  *
+ * This class contains methods that deal with reading in and manipulating bmp files. It can convert an image array from
+ * 2D to 1D and back, and can scale the pels so that they fall between zero and one. This is used so that the image can
+ * be inputted into the neural network.
+ *
+ * @author Chaitanya Ravuri
+ * @version October 24, 2019
  */
 public class ImageWrapper
 {
 
-   public int[][] imageArray;
+   public int[][] imageArray;                     // The matrix containing all the pels of the image
 
-   private final double SCALING_FACTOR = 1 << 24;
+   private final double SCALING_FACTOR = 1 << 24; // The amount to divide all pels by so that they are between 0 and 1
 
+   /**
+    * Creates a new ImageWrapper from a file. This constructor reads the image array from the file and masks off the
+    * alpha channel.
+    *
+    * @param fileName the bmp file containing the image
+    */
    public ImageWrapper(String fileName)
    {
       imageArray = DibDump.bmpToArray(fileName);
@@ -25,6 +33,14 @@ public class ImageWrapper
       }
    }
 
+   /**
+    * Creates a new ImageWrapper from a one-dimensional array and the height and width of the image. Converts the 1D
+    * array to a matrix using the height and width and multiplies it by the scaling factor.
+    *
+    * @param image  the 1D array containing the pels of the image
+    * @param height the height of the image
+    * @param width  the width of the image
+    */
    public ImageWrapper(double[] image, int height, int width)
    {
       imageArray = new int[height][width];
@@ -37,11 +53,21 @@ public class ImageWrapper
       }
    }
 
+   /**
+    * Creates a new ImageWrapper from an imageArray. Sets the imageArray instance variable to that imageArray.
+    *
+    * @param imageArray the 2D array containing the pels of the image
+    */
    public ImageWrapper(int[][] imageArray)
    {
       this.imageArray = imageArray;
    }
 
+   /**
+    * Converts the imageArray instance variable to a one-dimensional double array and divides by the scaling factor.
+    *
+    * @return the 1D image array
+    */
    public double[] toDoubleArray()
    {
       double[] imageDoubleArray = new double[imageArray.length * imageArray[0].length];
@@ -57,16 +83,31 @@ public class ImageWrapper
       return imageDoubleArray;
    }
 
+   /**
+    * Takes the imageArray instance variable and puts it in the given bmp file.
+    *
+    * @param fileName the file in which the image should go
+    */
    public void toBMP(String fileName)
    {
       DibDump.imageArrayToBMP(imageArray, fileName);
    }
 
+   /**
+    * Gets the height of the image array, or the number of rows
+    *
+    * @return the height of the image
+    */
    public int getHeight()
    {
       return imageArray.length;
    }
 
+   /**
+    * Gets the width of the image array, or the number of columns
+    *
+    * @return the width of the image
+    */
    public int getWidth()
    {
       return imageArray[0].length;
